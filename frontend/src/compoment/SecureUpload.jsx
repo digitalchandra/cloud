0dce23c15c1ab9579763e3d51c7bfa28bc53c2e5
Chandra Shrestha <78147911+digitalchandra@users.noreply.github.com>
2024-02-17 22:08:59 +0545

    enviroment setup on forntend

---
+++ frontend/src/compoment/SecureUpload.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SecureUpload() {
+  return (
+    <>
+        <h1> This is Secure Upload Page</h1>
+    </>
+  )
+}
---
+++ frontend/src/compoment/Uploads.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Uploads() {
+  return (
+    <>
+        <h3> This is the Upload Page </h3>
+    </>
+  )
+}
